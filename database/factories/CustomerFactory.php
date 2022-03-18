e0e0fce299d7a73a8d761d36039d394c07487830
Wilson Hung <whugntw@gmail.com>
2022-03-18 08:18:10 +0800

    0318: enable zh_TW, change fake content of customer EID

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -14,13 +14,14 @@ class CustomerFactory extends Factory {
 		return [
 			'name' => $this->faker->name,
 			'ear_no' => $this->faker->ean8,
-			'eid' => $this->faker->realText(30),
+			'eid' => $this->faker->ean13,
 			'contact' => $this->faker->name,
 			'birthday' => $this->faker->date($format = 'Y-m-d', $max = 'now'),
 			'gender' => $this->faker->randomElement($array = array('F', 'M')),
 			'address' => $this->faker->address,
 			'phone' => $this->faker->phoneNumber,
 			'email' => $this->faker->email,
+			'membership' => $this->faker->randomElement($array = array('臨時會員', '普通會員', '金質會員', '白金會員', '黑卡會員', '奥客')),
 			'userid' => $this->faker->userName,
 			'pwd' => $this->faker->password,
 		];
